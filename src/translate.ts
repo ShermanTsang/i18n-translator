ed91b9ac8ec8e1bf597df9ffb1e8ebe82fde3608
ShermanT <sharemant@163.com>
2024-07-12 16:36:07 +0800

    chore: remove unexpected commiting file from git VSC

--- src/translate.ts
+++
@@ -1,7 +0,0 @@
-import { DeepseekTranslator } from './providers/deepseek.ts'
-
-const translator = new DeepseekTranslator(
-  'sk-5805cac20c694c498aad2d5386e877fa',
-  './src/lang.json',
-)
-await translator.translate('fr')
